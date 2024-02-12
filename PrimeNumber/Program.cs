d1ba960a727d18bd2be654fafd88d308440eeec9
maqsad-muradovich <maqsad.baxadirov@gmail.com>
2024-02-12 19:23:29 +0500

    For funksiyasi orqali tub sonni topish algoritmi

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Sonni kiriting: ");
+int userInput = Convert.ToInt32(Console.ReadLine());
+int CountDividers = 0;
+
+for(int i = 2; i < userInput; i++){
+    if(userInput%i==0)
+        CountDividers++;
+}
+
+if(CountDividers==0)
+    Console.WriteLine("Tub son");
+else
+    Console.WriteLine("Tub son emas!");
