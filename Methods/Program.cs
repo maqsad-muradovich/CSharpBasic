6d75e5ab327a9b44f4b6854d796a5ee45b3901da
maqsad-muradovich <maqsad.baxadirov@gmail.com>
2024-02-16 20:08:08 +0500

    Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,9 @@
+﻿string GetUserInputWithPrompt(string str = "default"){
+    if(str!="default"){
+        Console.Write(str+" ");
+    }
+    return Console.ReadLine();
+}
+
+string str = input("Qiymat: ");
+Console.WriteLine(str);
