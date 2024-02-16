834672f2d80f43bb3e9f69e7654a1fbe53dd8caa
maqsad-muradovich <maqsad.baxadirov@gmail.com>
2024-02-16 21:26:03 +0500

    Ushbu kod C# tilida yozilgan konsol dasturida menyuni ko'rsatadi, unda uch turdagi tanlovlar mavjud: Triangle (uchburchak), Square (kvadrat) va Exit (chiquvchi). Foydalanuvchi biror bir variantni tanlash uchun raqam kiritishi kerak.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,9 +1,45 @@
-﻿string GetUserInputWithPrompt(string str = "default"){
-    if(str!="default"){
-        Console.Write(str+" ");
+﻿void ShowMenu()
+    {
+        Console.WriteLine("Menu:");
+        Console.WriteLine("1. Triangle");
+        Console.WriteLine("2. Square");
+        Console.WriteLine("3. Exit");
     }
-    return Console.ReadLine();
+
+void DrawSquare()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(new string('*', size));
+        }
+    }
+
+void DrawTriangle()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        Console.WriteLine(new string('* '));
+    }
 }
 
-string str = input("Qiymat: ");
-Console.WriteLine(str);
+do
+{
+    ShowMenu();
+    Console.Write("Tanlovingizni kiriting: ");
+    choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "1":
+            DrawTriangle();
+            break;
+        case "2":
+            DrawSquare();
+            break;
+        case "3":
+            Console.WriteLine("Hayr!)");
+            break;
+        default:
+            Console.WriteLine("Bunday variant yo'q. Boshqa variant kiritnig!");
+            break;
+    }
+} while (choice != "3");
